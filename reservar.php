270d818c6659d36805db61e1e5e95530b1ecb767
Sabor12-web <ma.emilia.enriquez@gmail.com>
2025-04-01 09:03:49 -0500

    Add files via upload

--- reservar.php
+++ reservar.php
@@ -1,19 +1,23 @@
 <?php
 include 'db.php'; // Asegúrate de tener el archivo db.php en el mismo directorio
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['name'];
-    $fecha = $_POST['date'];
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $numero_personas = $_POST['numero_personas'];
+    $mensaje = $_POST['mensaje'];
 
-    // Preparar y ejecutar la consulta SQL
-    $sql = "INSERT INTO reservaciones (nombre, fecha) VALUES ('$nombre', '$fecha')";
-    
+    $sql = "INSERT INTO reservaciones (nombre, email, telefono, fecha, hora, numero_personas, mensaje)
+            VALUES ('$nombre', '$email', '$telefono', '$fecha', '$hora', '$numero_personas', '$mensaje')";
+
     if ($conn->query($sql) === TRUE) {
-        echo "¡Reservación confirmada para $nombre el $fecha!";
+        echo "Reservación guardada con éxito.";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-    
-    $conn->close(); // Cerrar la conexión
+
+    $conn->close();
 }
 ?>
